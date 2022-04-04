627e241d6bca5a58386581d254cc221e02c808e0
post-rex <post-rex@pm.me>
2022-04-05 00:36:49 +0200

    giving up for tonight

--- test_bin/src/lib.rs
+++ test_bin/src/lib.rs
@@ -1,14 +1,17 @@
 use std::io::Write;
-use std::time::{Duration, Instant};
+use std::time::{Duration, Instant, UNIX_EPOCH};
 use fancy_std::time::sleep_for;
 use fancy_std::{log, yield_now};
 
 
 #[fancy_std::main]
 pub async fn main() {
-    log(0xDEADBEEF);
+    log(UNIX_EPOCH.elapsed().unwrap().as_secs());
+    log(0xDEAD0001);
     for i in 0.. {
+        log(0xDEAD0000 + i);
         println!("{}", i);
         sleep_for(Duration::from_secs(1)).await
     }
+    log(0xDEAD00FF);
 }
